2d13e41693b24d58396cc506bc325bbc269d4584
F-loat <chaimaoyuan@foxmail.com>
2021-03-31 22:02:45 +0800

    fix: #35 deep prop regexp

--- src/utils.js
+++ src/utils.js
@@ -837,7 +837,7 @@ function toKey(value) {
   return result == '0' && 1 / value == -INFINITY ? '-0' : result;
 }
 
-const reIsDeepProp = /\.|\[(?:[^[\]]*|(["'])(?:(?!\1)[^\\]|\\.)*?\1)\]/;
+const reIsDeepProp = /\/|\[(?:[^[\]]*|(["'])(?:(?!\1)[^\\]|\\.)*?\1)\]/;
 const reIsPlainProp = /^\w*$/;
 
 function isKey(value, object) {
